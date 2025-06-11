c5dae89a27fbfb8e6800e1d3666b13ca4450ec9c
mdlka <mogilevskiy499@gmail.com>
2025-06-12 01:38:59 +0300

    Refactor Graph

--- src/main/java/org/sfedu/pathfinder/model/Graph.java
+++ src/main/java/org/sfedu/pathfinder/model/Graph.java
@@ -1,20 +1,39 @@
 package org.sfedu.pathfinder.model;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Graph {
     private final Map<String, Node> nodes = new HashMap<>();
-    private final List<Edge> edges = new ArrayList<>();
-    private final Map<Node, List<Edge>> adjacencyMap = new HashMap<>();
+    private final Map<Node, List<Edge>> outEdges = new HashMap<>();
+    private final Map<Node, List<Edge>> inEdges = new HashMap<>();
 
     public void addNode(Node node) {
         nodes.put(node.id(), node);
-        adjacencyMap.putIfAbsent(node, new ArrayList<>());
+        outEdges.putIfAbsent(node, new ArrayList<>());
+        inEdges.putIfAbsent(node, new ArrayList<>());
     }
 
-    public void addEdge(Edge edge) {
-        edges.add(edge);
-        adjacencyMap.get(edge.from()).add(edge);
+    public void addEdge(Edge edge) throws IllegalArgumentException {
+        outEdges.get(edge.from()).add(edge);
+        inEdges.get(edge.to()).add(edge);
+    }
+
+    public void removeNodeById(String id) {
+        var node = nodes.remove(id);
+
+        if (node == null)
+            return;
+
+        for (Edge edge : outEdges.remove(node))
+            inEdges.get(edge.to()).remove(edge);
+
+        for (Edge edge : inEdges.remove(node))
+            outEdges.get(edge.from()).remove(edge);
     }
 
     public Node getNodeById(String id) {
@@ -26,11 +45,11 @@ public class Graph {
     }
 
     public List<Edge> getEdgesFrom(Node node) {
-        return adjacencyMap.getOrDefault(node, Collections.emptyList());
+        return outEdges.getOrDefault(node, Collections.emptyList());
     }
 
     public List<Node> getNeighbors(Node node) {
-        List<Node> neighbors = new ArrayList<>();
+        var neighbors = new ArrayList<Node>();
 
         for (Edge edge : getEdgesFrom(node))
             neighbors.add(edge.to());
@@ -51,18 +70,8 @@ public class Graph {
     }
 
     public int getEdgeCount() {
-        return edges.size();
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("Graph:\n");
-
-        for (Node node : nodes.values()) {
-            sb.append("  ").append(node).append(" -> ");
-            sb.append(getNeighbors(node)).append("\n");
-        }
-
-        return sb.toString();
+        return outEdges.values().stream()
+                .mapToInt(List::size)
+                .sum();
     }
 }
