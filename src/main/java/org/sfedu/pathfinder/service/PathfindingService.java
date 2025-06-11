b7d703f7d990bdec988c3bb55e62dbf3ff7ecc6b
mdlka <mogilevskiy499@gmail.com>
2025-06-12 01:47:11 +0300

    Refactor and optimize PathfindingService

--- src/main/java/org/sfedu/pathfinder/service/PathfindingService.java
+++ src/main/java/org/sfedu/pathfinder/service/PathfindingService.java
@@ -3,12 +3,11 @@ package org.sfedu.pathfinder.service;
 import org.sfedu.pathfinder.algorithm.Path;
 import org.sfedu.pathfinder.algorithm.PathfindingAlgorithm;
 import org.sfedu.pathfinder.io.MapDataReader;
-import org.sfedu.pathfinder.model.Edge;
 import org.sfedu.pathfinder.model.Graph;
 import org.sfedu.pathfinder.model.Node;
 import org.sfedu.pathfinder.utils.GraphUtils;
-import org.sfedu.pathfinder.utils.PMath;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
@@ -22,9 +21,11 @@ public class PathfindingService {
         this.algorithm = algorithm;
     }
 
-    public void loadGraph(MapDataReader mapDataReader, InputStream is) {
-        this.graph = mapDataReader.readGraph(is);
+    public void loadGraph(MapDataReader mapDataReader, InputStream is) throws IOException {
+        this.graph = mapDataReader.readGraph(MapDataReader.RoadsType.Vehicle, is);
         System.out.println("Graph loaded. Nodes: " + graph.getNodeCount() + ", Edges: " + graph.getEdgeCount());
+
+        removeExtraComponents();
     }
 
     public Path findPath(double startLat, double startLon, double targetLat, double targetLon) {
@@ -38,10 +39,6 @@ public class PathfindingService {
             return new Path(List.of());
 
         System.out.println("Is reachable - " + GraphUtils.isReachable(graph, start, target));
-
-        if (!GraphUtils.isReachable(graph, start, target))
-            connectComponents(start, target);
-
         System.out.println("Finding path from " + start + " to " + target);
 
         return algorithm.findShortestPath(graph, start, target);
@@ -51,116 +48,36 @@ public class PathfindingService {
         return graph;
     }
 
-    private void connectComponents(Node start, Node target) {
-        List<Set<Node>> components = findConnectedComponents(graph);
-        Integer startComponentIndex = getComponentIndex(components, start);
-        Integer endComponentIndex = getComponentIndex(components, target);
+    private void removeExtraComponents() {
+        List<Set<Node>> components = GraphUtils.findComponents(graph);
+        System.out.println("Components count - " + components.size());
 
-        System.out.println("Components before connection: " + components.size());
-
-        if (startComponentIndex == null || endComponentIndex == null) {
-            System.out.println("Одна из точек вне графа.");
-            return;
-        }
-
-        if (startComponentIndex.equals(endComponentIndex)) {
-            System.out.println("Точки находятся в одной компоненте связности.");
-        } else {
-            System.out.println("Точки находятся в разных компонентах связности.");
-            System.out.printf("Старт в компоненте #%d, финиш в компоненте #%d%n",
-                    startComponentIndex, endComponentIndex);
-
-            connectComponents(graph, components, 0.0001); // 10 метров. слишком много не надо, а то фигня получается
-
-            components = findConnectedComponents(graph);
-            System.out.println("Components after connection: " + components.size());
-            System.out.println("Узлов: " + graph.getNodeCount() + ", рёбер: " + graph.getEdgeCount());
-        }
-    }
-
-    private static List<Set<Node>> findConnectedComponents(Graph graph) {
-        Set<Node> visited = new HashSet<>();
-        List<Set<Node>> components = new ArrayList<>();
-
-        for (Node node : graph.getAllNodes()) {
-            if (!visited.contains(node)) {
-                Set<Node> component = new LinkedHashSet<>();
-                Deque<Node> stack = new ArrayDeque<>();
-
-                stack.push(node);
-
-                while (!stack.isEmpty()) {
-                    Node current = stack.pop();
-
-                    if (visited.contains(current))
-                        continue;
-
-                    visited.add(current);
-                    component.add(current);
-
-                    for (Node neighbor : graph.getNeighbors(current))
-                        if (!visited.contains(neighbor))
-                            stack.push(neighbor);
-                }
-
-                components.add(component);
-            }
-        }
-
-        return components;
-    }
-
-    private static Integer getComponentIndex(List<Set<Node>> components, Node node) {
-        for (int i = 0; i < components.size(); i++)
-            if (components.get(i).contains(node))
-                return i;
-
-        return null;
-    }
-
-    private static Node[] findClosestPair(Set<Node> comp1, Set<Node> comp2) {
-        double minDistance = Double.MAX_VALUE;
-        Node[] result = new Node[2];
-
-        for (Node n1 : comp1) {
-            for (Node n2 : comp2) {
-                double distance = PMath.calculateDistance(n1.latitude(), n1.longitude(), n2.latitude(), n2.longitude());
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    result[0] = n1;
-                    result[1] = n2;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    private static void connectComponents(Graph graph, List<Set<Node>> components, double maxAllowedDistance) {
-        int count = 0;
+        int maxIndex = findComponentIndexWithMaxSize(components);
 
         for (int i = 0; i < components.size(); i++) {
-            for (int j = i + 1; j < components.size(); j++) {
-                Set<Node> compA = components.get(i);
-                Set<Node> compB = components.get(j);
+            if (i == maxIndex)
+                continue;
 
-                Node[] closestPair = findClosestPair(compA, compB);
-                Node from = closestPair[0];
-                Node to = closestPair[1];
-                double distance = PMath.calculateDistance(from.latitude(), from.longitude(), to.latitude(), to.longitude());
-
-                if (distance <= maxAllowedDistance) {
-                    String edgeId = "artificial-" + i + "-" + j;
-                    graph.addEdge(new Edge(edgeId, from, to, distance));
-                    graph.addEdge(new Edge("reverse-" + edgeId, to, from, distance));
-                    count++;
-                    // System.out.printf("Добавлено искусственное ребро между компонентами %d и %d (расстояние: %.5f)%n", i, j, distance);
-                } else {
-                    // System.out.printf("Компоненты %d и %d слишком далеки (%.5f), ребро не добавлено.%n", i, j, distance);
-                }
-            }
+            for (Node node : components.get(i))
+                graph.removeNodeById(node.id());
         }
 
-        System.out.println("Добавлено искусственных рёбер: " + count);
+        System.out.println("Components count - " + GraphUtils.findComponents(graph).size());
+        System.out.println("Graph cleaned. Nodes: " + graph.getNodeCount() + ", Edges: " + graph.getEdgeCount());
+    }
+
+    private static int findComponentIndexWithMaxSize(List<Set<Node>> components) {
+        int index = -1;
+        int maxSize = -1;
+
+        for (int i = 0; i < components.size(); i++) {
+            int size = components.get(i).size();
+
+            if (size > maxSize) {
+                maxSize = size;
+                index = i;
+            }
+        }
+        return index;
     }
 }
--- src/main/java/org/sfedu/pathfinder/utils/GraphUtils.java
+++ src/main/java/org/sfedu/pathfinder/utils/GraphUtils.java
@@ -3,12 +3,7 @@ package org.sfedu.pathfinder.utils;
 import org.sfedu.pathfinder.model.Graph;
 import org.sfedu.pathfinder.model.Node;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
-
-import static org.sfedu.pathfinder.utils.PMath.calculateDistance;
+import java.util.*;
 
 public class GraphUtils {
     public static boolean isReachable(Graph graph, Node start, Node target) {
@@ -40,7 +35,7 @@ public class GraphUtils {
         double minDistance = Double.MAX_VALUE;
 
         for (Node node : graph.getAllNodes()) {
-            double distance = calculateDistance(lat, lon, node.latitude(), node.longitude());
+            double distance = PMath.calculateDistance(lat, lon, node.latitude(), node.longitude());
 
             if (distance < minDistance && distance <= maxRadiusMeters) {
                 minDistance = distance;
@@ -49,5 +44,37 @@ public class GraphUtils {
         }
 
         return nearest;
+    }
+
+    public static List<Set<Node>> findComponents(Graph graph) {
+        Set<Node> visited = new HashSet<>();
+        List<Set<Node>> components = new ArrayList<>();
+
+        for (Node node : graph.getAllNodes()) {
+            if (!visited.contains(node)) {
+                Set<Node> component = new LinkedHashSet<>();
+                Deque<Node> stack = new ArrayDeque<>();
+
+                stack.push(node);
+
+                while (!stack.isEmpty()) {
+                    Node current = stack.pop();
+
+                    if (visited.contains(current))
+                        continue;
+
+                    visited.add(current);
+                    component.add(current);
+
+                    for (Node neighbor : graph.getNeighbors(current))
+                        if (!visited.contains(neighbor))
+                            stack.push(neighbor);
+                }
+
+                components.add(component);
+            }
+        }
+
+        return components;
     }
 }
